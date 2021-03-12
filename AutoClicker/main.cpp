96952135d7318604678417223ee660e03b10f4ed
bkepps <benskepp@gmail.com>
2021-03-11 19:08:56 -0600

    removed commented code

--- AutoClicker/main.cpp
+++ AutoClicker/main.cpp
@@ -2,9 +2,7 @@
 #include <Windows.h>
 
 int main() {
-	short keyState;
 	for (;;) {
-		//keyState = GetAsyncKeyState(VK_LSHIFT);
 		if (GetAsyncKeyState(VK_LSHIFT) && GetAsyncKeyState(VK_CONTROL)) {
 			mouse_event(MOUSEEVENTF_LEFTDOWN, 0x0, 0x0, 0x1, NULL);
 			std::cout << "down" << std::endl;
@@ -12,7 +10,6 @@ int main() {
 			std::cout << "up" << std::endl;
 			Sleep(1000);
 		}
-		//std::cout << std::hex << (GetAsyncKeyState(VK_LSHIFT) && GetAsyncKeyState(VK_CONTROL)) << std::endl << std::endl;
 		Sleep(200);
 	}
 	return 0;
